34e1d2a82ffd635d0777a95c331778c9b2158daf
KaLi <36546318+KaLi-1010@users.noreply.github.com>
2018-04-04 20:53:45 +0200

    Create tester_frt.py

---
+++ tester_frt.py
@@ -0,0 +1,17 @@
+import requests
+from flask import Flask, render_template, request
+
+URL = 'https://api.fortnitetracker.com/v1/profile/pc/Twitch_Svennoss'
+
+#Diccionario con datos de la api para acceder
+headers = {'TRN-Api-Key' : '57e6a8ad-8fc9-4189-9fa6-6ac46a095231'}
+
+res = requests.get(URL, headers=headers)
+
+print (res.text)
+
+# Dado que la API no tiene demasiada información sobre su uso,
+# tenemos que ver primero como nos va a dar los datos.
+#--------------------------------------------------------------------------------
+# Para ello, haremos na petición al servidor pidiendole los headers (datos) 
+# de un jugador, en este caso hardcodeamos el jugador en la URL para hacer pruebas.
